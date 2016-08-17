887fb1b631cc3e149f23a333c6869861879d612c
Stefan Ruh <ruh@abgeordnetenwatch.de>
2016-08-17 12:35:02 +0200

    display question and answer count in search results and sort by it

--- sites/all/themes/custom/parliamentwatch/templates/views-view-fields--politician-search-view--page.tpl.php
+++ sites/all/themes/custom/parliamentwatch/templates/views-view-fields--politician-search-view--page.tpl.php
@@ -32,23 +32,8 @@ $parliament = taxonomy_term_load($row->_entity_properties['entity object']->fiel
 
 #$constituency = taxonomy_term_load($row->_entity_properties['entity object']->field_user_constituency['und'][0]['tid'])->name;
 
-$uid = $row->_entity_properties['uid'];
-$roles = $elements['#account']->roles; //TODO: Aus Entity-Object laden
-if (in_array("Candidate",$roles)){
-  $role = "candidate";
-}
-else{
-  $role = "deputy";
-}
-$result = db_query('SELECT `vid` FROM `user_archive_cache` WHERE `uid` = :uid AND `parliament_name` = :parliament AND `user_role` = :role',array(
-  ':uid' => $uid,
-  ':parliament' => $parliament,
-  ':role' => $role
-))->fetchCol();
-$user_revision = user_revision_load($uid,$result[0]);
-
-$questions_get = $user_revision->field_user_questions_get['und'][0]['value'];
-$answers_give = $user_revision->field_user_answers_give['und'][0]['value'];
+$questions_get = $row->_entity_properties['field_user_questions_get'];
+$answers_give = $row->_entity_properties['field_user_answers_give'];
 ?>
 <hr/>
 <div class="ds-1col <?php print $classes; ?> clearfix">
@@ -62,7 +47,8 @@ $answers_give = $user_revision->field_user_answers_give['und'][0]['value'];
   <div id="user_user_search_result_group_grid_6" class="field-group-format group_grid_6 field-group-div group-grid-6 grid-6 omega speed-fast effect-none">
     <?php if($has_title): ?><strong class="field-title"><?php print $title;?>&nbsp;</strong><?php endif; ?><strong class="field-fname"><?php print $fname;?>&nbsp;</strong><strong class="field-lname"><?php print $lname;?>,&nbsp;</strong><?php print $party;?>
     <div class="small"><?php print $parliament; print (!empty($constituency))?"&nbsp;&ndash;&nbsp;".$constituency:"";?></div>
- </div>
+    <span class="field-questionsget"><?php print $questions_get;?> <?php print ($questions_get==1)?'Frage':'Fragen';?></span><?php print $answers_give;?> <?php print ($answers_give==1)?'Antwort':'Antworten';?>
+</div>
   <div class="text-right">
         <span class="link-profile"><a href="<?php print $profile_link;?>">Profil öffnen</a>
         </span>
