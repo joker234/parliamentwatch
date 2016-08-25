7dfda412c4daec133d5018362d758b3164324129
Boris Hekele <hekele@parliamentwatch.org>
2016-08-25 16:08:19 +0200

    AW-3482 AW-3547 question and answers statistic by party

---
+++ sites/all/modules/custom/pw_dialogues/question-answer-statistic.tpl.php
@@ -0,0 +1,6 @@
+Partei: <?php print $party; ?><br />
+Anzahl Politiker: <?php print $count_politcians; ?><br />
+Anzahl Fragen: <?php print $count_questions; ?><br />
+Anzahl Antworten: <?php print $count_answers; ?><br />
+Antwortqoute: <?php print $quotient; ?><br />
+Top Party: <?php print $is_top_party; ?><br /><br />
