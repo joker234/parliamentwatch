e14bf103bed9eedcb64855765fdb22d9cf6232e5
Christopher Neugebauer <christopher.neugebauer.87@googlemail.com>
2016-09-07 13:23:02 +0200

    small fixes for content paragraph-bundle

--- sites/all/themes/custom/parliamentwatch/templates/newsletter/ds-1col--paragraphs-item-paragraphs-pw-content.tpl.php
+++ sites/all/themes/custom/parliamentwatch/templates/newsletter/ds-1col--paragraphs-item-paragraphs-pw-content.tpl.php
@@ -3,17 +3,11 @@
 <?php endif; ?>
 <div class="<?php print render($content['field_pg_donate_targetgroup']); ?>">
 
-
-  <?php if (!empty($content['field_pg_content_link'])): ?>
-  <div class="social-media">
-    <a href="https://www.facebook.com/sharer/sharer.php?u=<?php print $node_url; ?>" class="facebook" target="_blank">facebook</a>
-    <a href="https://twitter.com/intent/tweet?text=<?php print $field_pg_content_title[0]['safe_value']; ?>&url=<?php print $node_url; ?>" class="twitter" target="_blank">twitter</a>
-  </div>
-  <?php endif; ?>
   <?php hide($content['field_pg_class']); ?>
   <?php hide($content['field_pg_content_title']); ?>
   <?php hide($content['field_pg_content_img']); ?>
-  
+  <?php hide($content['field_pg_content_link']); ?>
+
 
     <table cellpadding="0" cellspacing="0" border="0" bgcolor="#ffffff" width="600" align="center" class="deviceWidth">
         <tr>
@@ -25,15 +19,17 @@
                     <tr>
                         <td width="80%">
                             <?php if (!empty($content['field_pg_content_title'])): ?>
-                            <h3 style="font-family: 'Times New Roman', Times, serif; font-weight: normal; font-size: 24px; margin: 0;"><a target="_blank" href="Deutschland droht erneute Rüge durch Korruptionswächter" style="color: #333; text-decoration: none;"><?php print render($content['field_pg_content_title']); ?></a></h3>
+                            <h3 style="font-family: 'Times New Roman', Times, serif; font-weight: normal; font-size: 24px; margin: 0;">
+                                <?php if (!empty($content['field_pg_content_link'])): ?><a target="_blank" href="<?php print $field_pg_content_link[0]['url'] ?>" style="color: #333; text-decoration: none;"><?php endif; ?>
+                                    <?php print render($content['field_pg_content_title']); ?>
+                                <?php if (!empty($content['field_pg_content_link'])): ?></a><?php endif; ?>
+                            </h3>
                             <?php endif; ?>
                         </td>
                         <td width="20%" style="text-align: right;">
-                            <?php if (!empty($content['field_pg_content_link'])): ?>
-                                <div class="social-media">
-                                    <a href="https://www.facebook.com/sharer/sharer.php?u=<?php print $node_url; ?>" class="facebook" target="_blank"><img src="<?php print $GLOBALS['base_url'] ?>/sites/all/themes/custom/parliamentwatch/images/newsletter/social-facebook.png" alt="Facebook" border="0" style="display: inline-block;"></a>
-                                    <a href="https://twitter.com/intent/tweet?text=<?php print $field_pg_content_title[0]['safe_value']; ?>&url=<?php print $node_url; ?>" class="twitter" target="_blank"><img src="<?php print $GLOBALS['base_url'] ?>/sites/all/themes/custom/parliamentwatch/images/newsletter/social-twitter.png" alt="Twitter" border="0" style="display: inline-block;"></a>
-                                </div>
+                            <?php if (!empty($field_pg_content_link)): ?>
+                                <a href="https://www.facebook.com/sharer/sharer.php?u=<?php print $node_url; ?>" class="facebook" target="_blank"><img src="<?php print $GLOBALS['base_url'] ?>/sites/all/themes/custom/parliamentwatch/images/newsletter/social-facebook.png" alt="Facebook" border="0" style="display: inline-block;"></a>
+                                <a href="https://twitter.com/intent/tweet?text=<?php print $field_pg_content_title[0]['safe_value']; ?>&url=<?php print $node_url; ?>" class="twitter" target="_blank"><img src="<?php print $GLOBALS['base_url'] ?>/sites/all/themes/custom/parliamentwatch/images/newsletter/social-twitter.png" alt="Twitter" border="0" style="display: inline-block;"></a>
                             <?php endif; ?>
                         </td>
                     </tr>
@@ -47,6 +43,11 @@
             <?php if (!empty($content['field_pg_content_img'])): ?>
                 <td colspan="3" width="100%">
                     <?php print render($content); ?>
+                    <?php if (!empty($field_pg_content_link)): ?>
+                        <a target="_blank" href="<?php print $field_pg_content_link[0]['url'] ?>" style="font-family: Arial, Helvetica, Sans-Serif; color: #f63; text-decoration: none; font-weight: bold;">
+                            <img src="http://dev-newsletter.neugebauer.aw.local/sites/all/themes/custom/parliamentwatch/images/newsletter/link-icon.png" width="12" height="12" border="0" style="display:inline-block; margin-bottom: -1px;"> <?php print $field_pg_content_link[0]['title'] ?>
+                        </a>
+                    <?php endif; ?>
                 </td>
             <?php else: ?>
                 <td width="240" class="block_td pic_td" style="vertical-align: top;">
@@ -55,6 +56,11 @@
                 <td width="20" class="block_td pic_td" style="vertical-align: top;">&nbsp;</td>
                 <td width="340" class="block_td percent_td" style="vertical-align: top;">
                     <?php print render($content); ?>
+                    <?php if (!empty($field_pg_content_link)): ?>
+                        <a target="_blank" href="<?php print $field_pg_content_link[0]['url'] ?>" style="font-family: Arial, Helvetica, Sans-Serif; color: #f63; text-decoration: none; font-weight: bold;">
+                            <img src="http://dev-newsletter.neugebauer.aw.local/sites/all/themes/custom/parliamentwatch/images/newsletter/link-icon.png" width="12" height="12" border="0" style="display:inline-block; margin-bottom: -1px;"> <?php print $field_pg_content_link[0]['title'] ?>
+                        </a>
+                    <?php endif; ?>
                 </td>
             <?php endif; ?>
         </tr>
